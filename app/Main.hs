280f51a1672580ea2ec739e65cb0197d9423d5d2
Richard Bilson <rcbilson@gmail.com>
2025-04-15 12:19:29 -0400

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -75,39 +75,8 @@ consumeToken calc token =
             Nothing -> error ("Unknown operator: " ++ op)
         Num n -> calc{engine=push (engine calc) n}
 
-consumeTokenList :: Calculator a b -> [Token a] -> Calculator a b
-consumeTokenList calculator tokens = foldl consumeToken calculator tokens
-
-splitWord :: String -> (String, String)
-splitWord "" = ("", "")
-splitWord (' ':xs) = ("", xs)
-splitWord ('\n':xs) = ("", xs)
-splitWord (t:xs) = case splitWord xs of (str, rest) -> (t:str, rest)
-
-parseToken :: Read a => Calculator a b -> String -> (Maybe (Token a), String)
-parseToken calc str =
-    let (headWord, rest) = splitWord str
-    in
-        if Map.member headWord (opsMap calc) then (Just (Op headWord), rest)
-        else case (readNum calc) str of
-            [] -> (Nothing, str)
-            (num,rest):_ -> (Just (Num num), rest)
-
-parseTokenList :: Read a => Calculator a b -> String -> [Token a]
-parseTokenList calc "" = []
-parseTokenList calc str =
-    let (headWord, afterWord) = splitWord str
-        readResult = (readNum calc) str
-    in
-        case readResult of
-            (num, afterNum):_ -> (Num num):(parseTokenList calc afterNum)
-            [] ->
-                if Map.member headWord (opsMap calc) then (Op headWord):(parseTokenList calc afterWord)
-                else parseTokenList calc afterWord
-
 tryParse :: Calculator a b -> [Char] -> (Maybe (Token a), [Char])
 tryParse calc [] = (Nothing, [])
-tryParse calc r@(' ':rest) = tryParse calc r
 tryParse calc str =
     case (readNum calc) str of
         (num, rest):_ -> (Just (Num num), rest)
@@ -118,6 +87,10 @@ tryParse calc str =
 parseChar :: Calculator a b -> [Char] -> Char -> (Maybe (Token a), [Char])
 parseChar calc acc ' ' = case tryParse calc acc of (t, r) -> (t, [])
 parseChar calc acc '\n' = case tryParse calc acc of (t, r) -> (t, [])
+parseChar calc [] '\177' = (Nothing, [])
+parseChar calc acc '\177' = case reverse acc of x:xs -> (Nothing, reverse xs)
+parseChar calc [] '\008' = (Nothing, [])
+parseChar calc acc '\008' = case reverse acc of x:xs -> (Nothing, reverse xs)
 parseChar calc acc c =
     let
         newacc = reverse (c:(reverse acc))
@@ -127,9 +100,9 @@ parseChar calc acc c =
         (Just (Num _), []) -> (Nothing, newacc)
         (t, r) -> (t, r)
 
-newCalculator :: (Show a, Show b, Read a) => Calculator a b -> [Char] -> [Char] -> IO ()
-newCalculator initialCalc acc [] = return ()
-newCalculator initialCalc acc (x:xs) =
+doCalculator :: (Show a, Show b, Read a) => Calculator a b -> [Char] -> [Char] -> IO ()
+doCalculator initialCalc acc [] = return ()
+doCalculator initialCalc acc (x:xs) =
     let (newCalc, newAcc) = case parseChar initialCalc acc x of
             (Nothing, rest) -> (initialCalc, rest)
             (Just t, rest) -> 
@@ -147,16 +120,9 @@ newCalculator initialCalc acc (x:xs) =
         putStrLn ""
         print newCalc
         putStr ("> " ++ newAcc)
-        newCalculator newCalc newAcc xs
-
-doCalculator :: (Show a, Show b, Read a) => Calculator a b -> [Token a] -> IO ()
-doCalculator initialCalc [] = return ()
-doCalculator initialCalc (token:tokens) = do
-    let newCalc = consumeToken initialCalc token
-    print newCalc
-    doCalculator newCalc tokens
+        doCalculator newCalc newAcc xs
 
 main :: IO ()
 main = do
     input <- getContents
-    newCalculator floatCalculator "" input
+    doCalculator floatCalculator "" input
